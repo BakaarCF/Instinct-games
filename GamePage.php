e61783e3d67a15bde843ce21f5e3790f5b1ec49c
BakaarCF <73856296+BakaarCF@users.noreply.github.com>
2021-05-08 06:06:46 +0200

    GamePageandContactUs

---
+++ GamePage.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="layouts/css/style.css">
+    <script src="https://kit.fontawesome.com/b1361fb5d5.js" crossorigin="anonymous"></script>
+    <title>GamePage | Instinct Games</title>
+</head>
+<body>
+<?php include("includes/nav.php");
+    ?>
+    <h1>WELCOME PLAY EVERYONE <span style="color:#30593D">Ark: Survival Evolved!</span></h1>
+    <p>Ark: Survival Evolved (stylized as ΛRK) is a 2017 action-adventure survival video game developed by Studio Wildcard, in collaboration with Instinct Games, Efecto Studios, and Virtual Basement. In the game, players must survive being stranded on an island filled with roaming dinosaurs and other prehistoric animals, natural hazards, and potentially hostile human players.
+The game is played from either a third-person or first-person perspective and its open world is navigated on foot or by riding a prehistoric animal. Players can use firearms and improvised weapons to defend against hostile humans and creatures, with the ability to build bases as defense on the ground and on some creatures. The game has both single-player and multiplayer options. Multiplayer allows the option to form tribes of members in a server. The max number of tribe mates varies from each server. In this mode all tamed dinosaurs and building structures are usually shared between the members. There is a PvE mode where players cannot fight each other.
+Development began in October 2014, where it was first released on PC as an early access title in the middle of 2015. The development team conducted research into the physical appearance of the animals, but took creative license for gameplay purposes. Egypt-based developer Instinct Games was hired to facilitate the game's development. The game was released in August 2017 for PlayStation 4, Xbox One, Microsoft Windows, OS X and Linux, with versions for Android, iOS, and Nintendo Switch in 2018; a version for Stadia is scheduled for release in early 2021. It received generally mixed reviews, with criticism for its "punishing" difficulty, reliance on grinding, and performance issues, particularly on the Nintendo Switch version. Several expansions to the game have been released as downloadable content. The game begot two spin-off games in March 2018—virtual reality game Ark Park and sandbox survival game PixArk—and two companion apps: A-Calc in October 2015, and Dododex in August 2017.</p>
+<br>
+
+<h1>GOOD TO KNOW</h1>
+<br>
+<div class="gameinfo">
+<ul>
+    <li>
+        <span>Category:</span>
+        "Vertical Shooter"
+    </li>
+    <li>
+        <span>Requirments:</span>
+        "PS4, XBOXONE, Steam"
+    </li>
+    <li>
+        <span>Multiplayer:</span>
+        "Games Controller System"
+    </li>
+    <li>
+        <span>Visual Effect:</span>
+        "Operating System"
+    </li>
+
+</ul>
+</div>
+<h4 style="font-size: 24px; color: white; text-transform: uppercase; margin-bottom: 0;">GAMEPLAY<h4>
+    <br>
+<p>Ark: Survival Evolved is an action-adventure survival game.</br> Set in an open world environment with a dynamic day-night cycle and </br>played either from a third-person or first-person perspective. To survive, players must</br> establish a base, with a fire and weapons;additional activities, such as taming and feeding </br>dinosaurs, require more resources. The game's world, known as the "Ark", is approximately</br> 48 km2 (19 sq mi) in size: there is approximately 36 km2 (14 sq mi) of land with 12 km2 (4.6 sq mi) of ocean.</br>
+There are currently 176 creatures that populate the world of Ark. In the early versions of the game, nearly all</br> creatures were real dinosaurs and other prehistoric creatures, however, as the storyline progressed.
+    
+<div class="grid">
+<table >
+<tr>
+    <td><img src="layouts/img/3.png" height="200px" width="200px"/></td>
+    <td><img src="layouts/img/4.jpg" height="200px" width="200px"/></td>
+</tr>
+<tr>
+    <td><img src="layouts/img/5.jpg" height="200px" width="200px"/></td>
+    <td><img src="layouts/img/1.jpg" height="200px" width="200px"/></td>
+</tr>
+</table>
+
+
+
+</div>
+    
+    
+    
+    
+    
+    
+    
+    <?php include("includes/footer.php");
+    ?>
+</body>
+</html>
