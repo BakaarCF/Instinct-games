190bad43476dd073d6ae79c4914e457f8edbbd4f
youssef1810450 <youssef1810450@miuegypt.edu.eg>
2021-05-09 05:53:33 +0200

    news.php

---
+++ news.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="layouts/css/news.css">
+    
+    <title>Document</title>
+</head>
+
+<body style="background-color:black;">
+<?php include("includes/nav.php")?>
+
+<div class="bahgat">
+    <h1 style="font-size:80px;">RECENTS EVENTS &<br>ANNOUNCMENTS</h1>
+</div>
+<div class="containerr">
+    <h1>ATLAS</h1>
+    <h3>TRADE WINDS UPDATE INSTRUCTIONS FOR PRIVATE SERVERS!</h3>
+    <img class="soura" src="173-1739047_atlas-game-xbox-one.jpg" height="200" width="450" />
+    
+
+</div>
+<div class="container1">
+    <h1>ATLAS</h1>
+    <h3>PATCH 523.8 - LAND CLAIM CHANGES AND OTHER IMPROVEMENTS</h3>
+    <img class="soura1" src="apps.58949.14571142807322667.df9fc94b-3bd3-4ec2-b7a2-423331d84b17.jfif" height="200" width="450" />
+    
+</div>
+<div class="container2">
+    <h1>ARK: Survival Evolved</h1>
+    <h3>Don't be fooled by Noglin's adorable looks. Behind that cute face is a mind-bending surprise - the ability to take control of enemy creatures, tames, or even other humans!</h3>
+    <img class="soura1" src="dd.jfif" height="200" width="450" />
+    
+</div>
+<div class="container3">
+<h1>ARK: Survival Evolved</h1>
+    <h3>Don't be fooled by Noglin's adorable looks. Behind that cute face is a mind-bending surprise - the ability to take control of enemy creatures, tames, or even other humans!</h3>
+    <img class="soura1" src="ark-survival-evolved-xk.jpg" height="200" width="450" />   
+</div>
+
+</body>
+
+</html>
