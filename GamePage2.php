fe9fbbb64943804e1074e44fb717c93a9bf82d87
BakaarCF <73856296+BakaarCF@users.noreply.github.com>
2021-06-15 01:33:11 +0200

    save

--- GamePage2.php
+++ GamePage2.php
@@ -7,12 +7,15 @@
     <link rel="stylesheet" href="layouts/css/style.css">
     <script src="https://kit.fontawesome.com/b1361fb5d5.js" crossorigin="anonymous"></script>
     <title>GamePage | Instinct Games</title>
+    <link rel="icon" href="images/Bakaar.png" type="image/x-icon">
 </head>
 <body>
 <?php include("includes/nav.php");
     ?>
-    <h1>WELCOME PLAY EVERYONE <span style="color:#30593D">ATLAS!</span></h1>
-    <p>A massively multiplayer first-and-third-person fantasy pirate adventure. ATLAS will host up to 40,000 players exploring the same Globe simultaneously, with an unprecedented scale of cooperation and conflict! Stake your claim in this endless open world as you conquer territory, construct ships, search for buried treasure, assemble forts, plunder settlements and hire crew to join your powerful growing armada. Start small then expand your spheres of influence from a small island, up to an unstoppable pirate empire that spans across the oceans. Wage battle against enemy fleets as you singlehandedly can command large ships of war using the captaining system (or divide up to the responsibilities among your trusted lieutenants), or take control of any weapon directly with your own character. Dive deep into the briny water to explore permanent sunken wrecks and recover salvage, unearth the loot from procedurally-generated Treasure Maps and challenge zones, or complete challenging main questlines. Team up with other aspiring adventurers and sail into the vast ocean to discover new lands rich with region-specific elements, tame exotic natural and mythical creatures, raid forgotten tombs, confront powerful ancient gods and even build and administer your own colonies, cities, and civilizations to dominate the ATLAS in this ultimate quest for fortune and glory!.<span style="color:#30593D">Co-developed by Grapeshot Games.</span> </p>
+        <div class="game-container">
+    <div class="game-content">
+    <h1>WELCOME PLAY FOR EVERYONE <span style="color:#30593D">ATLAS!</span></h1>
+    <p style="color:#999;">A massively multiplayer first-and-third-person fantasy pirate adventure. Start small then expand your spheres of influence from a small island, up to an unstoppable pirate empire that spans across the oceans. Wage battle against enemy fleets as you singlehandedly can command large ships of war using the captaining system, or take control of any weapon directly with your own character. Dive deep into the briny water to explore permanent sunken wrecks and recover salvage, unearth the loot from procedurally-generated Treasure Maps and challenge zones, or complete challenging main questlines. Team up with other aspiring adventurers and sail into the vast ocean to discover new lands rich with region-specific elements, tame exotic natural and mythical creatures, raid forgotten tombs, confront powerful ancient gods and even build and administer your own colonies, cities, and civilizations to dominate the ATLAS in this ultimate quest for fortune and glory!.<span style="color:#30593D">Co-developed by Grapeshot Games.</span> </p>
 <br>
 
 <h1>GOOD TO KNOW</h1>
@@ -40,7 +43,7 @@
 </div>
 <h4 style="font-size: 24px; color: white; text-transform: uppercase; margin-bottom: 0;">GAMEPLAY<h4>
     <br>
-<p>From the creators of ARK - Survival Evolved comes ATLAS - a massively multiplayer first-and-third-person fantasy pirate adventure. ATLAS will host up to 40,000 players exploring the same Globe simultaneously, with an unprecedented scale of cooperation and conflict! Stake your claim in this endless open world as you conquer territory, construct ships, search for buried treasure, assemble forts, plunder settlements and hire crew to join your powerful growing armada. Start small then expand your spheres of influence from a small island, up to an unstoppable pirate empire that spans across the oceans. Wage battle against enemy fleets as you singlehandedly command large ships of war using the captaining system (or divide up to the responsibilities among your trusted lieutenants), or take control of any weapon directly with your own character. Dive deep into the briny water to explore permanent sunken wrecks
+<p style="color:#999;">ATLAS will host up to 40,000 players exploring the same Globe simultaneously, with an unprecedented scale of cooperation and conflict! Stake your claim in this endless open world as you conquer territory, construct ships, search for buried treasure, assemble forts, plunder settlements and hire crew to join your powerful growing armada. Start small then expand your spheres of influence from a small island, up to an unstoppable pirate empire that spans across the oceans. Wage battle against enemy fleets as you singlehandedly command large ships of war using the captaining system.</p>
     
 
 <div class="overview-history">
@@ -48,16 +51,17 @@
 <li style="margin: 0 auto;
   margin-top: 50px;">
 <div class="overview-history-videoo">
-    <a href="https://www.youtube.com/watch?v=miwYMiEUYiA"><img src="layouts/img/LOGO.jpg" style="vertical-align: middle;
-  border-style: none; width:105px; height:70px;" ></a> 
+    <a href="https://www.youtube.com/watch?v=miwYMiEUYiA"><img src="layouts/img/AtlasLogo.jpg" style="vertical-align: middle;
+  border-style: none; width:105px; height:6%;" ><i class="far fa-play-circle"></i></a> 
 </div>
 <div class="overview-history-content">
    
     <h4>
-        <a href="#">Release Date</a>
+        <a style="color:#999;" href="#">Release Date</a>
     </h4>
-    <span>December 22, 2018</span>
+    <span style="color:#999;">December 22, 2018</span>
 </div>
+<span style="color:#999;">ATLAS Extended-Length Gameplay Trailer!</span>
 </li>
 </ul>
 </div>
@@ -66,9 +70,9 @@
 
 <div class="Platforms" style="margin-top:20px;">
     <ul>
-        <li>
+        <li style="list-style:none;">
             <a href="https://store.steampowered.com/app/834910/ATLAS/">
-            <button style="padding:12px 18px; background-color:#30593D">Steam </button>
+            <button class="platforms-button" >Steam </button>
             </a>
             
         </li>
@@ -77,15 +81,20 @@
 <h4 style="font-size: 24px; color: white; text-transform: uppercase; margin-top: 20px;">ScreenShots<h4> 
 <div class="grid">
 <table >
+<table >
 <tr>
-    <td><img src="layouts/img/11.jpg" height="200px" width="200px"/></td>
-    <td><img src="layouts/img/22.jpg" height="200px" width="200px"/></td>
+    <td><a href="layouts/img/Atlas1.jpg" target="_blank"><img src="layouts/img/Atlas1.jpg" height="300px" width="100%"/></a></td>
+    <td><a href="layouts/img/Atlas2.jpg" target="_blank"><img src="layouts/img/Atlas2.jpg" height="300px" width="100%"/></a></td>
+    <td><a href="layouts/img/Atlas3.jpg" target="_blank"><img src="layouts/img/Atlas3.jpg" height="300px" width="100%"/></a></td>
 </tr>
 <tr>
-    <td><img src="layouts/img/33.jpg" height="200px" width="200px"/></td>
-    <td><img src="layouts/img/44.jpg" height="200px" width="200px"/></td>
+    <td><a href="layouts/img/Atlas4.jpg" target="_blank"><img src="layouts/img/Atlas4.jpg" height="300px" width="100%"/></a></td>
+    <td><a href="layouts/img/Atlas5.jpg" target="_blank"><img src="layouts/img/Atlas5.jpg" height="300px" width="100%"/></a></td>
+    <td><a href="layouts/img/Atlas6.jpg" target="_blank"><img src="layouts/img/Atlas6.jpg" height="300px" width="100%"/></a></td>
 </tr>
 </table>
+</div>
+</div>
 </div>
     <?php include("includes/footer.php");
     ?>
--- includes/nav.php
+++ includes/nav.php
@@ -2,7 +2,7 @@
 <style>
     
 .menu-btn {
-   background-color: #040008;
+   background-color: #000;
    color: white;
    padding: 16px;
    font-family:  "Montserrat",sans-serif;;
@@ -17,23 +17,27 @@
    position: absolute;
    min-width: 160px;
    z-index: 1;
+   background-color: #30593D;
+   color: white;
 }
 .links,.links-hidden{
    display: inline-block;
-   color: black;
+   color: white;
    padding: 12px 16px;
    text-decoration: none;
-   font-size: 18px;
+   font-size: 14px;
    font-weight: bold;
 }
 .links-hidden{
    display: block;
+   transition: .3s all;
 }
 .links{
    display: inline-block;
 }
 .links-hidden:hover,.links:hover {
-   background-color: #30593D;
+   background-color: black;
+
 }
 .dropdown-menu:hover .menu-content {
    display: block;
@@ -52,23 +56,23 @@
         <nav>
             <ul class="nav_links">
                 <li><a href="index.php">HOME</a></li>
-                <li><a href="#">NEWS</a></li>
-                <li><a href="profile.php">PROFILE</a></li>
-                <li><a href="#">ABOUT</a></li>
+                <li><a href="news.php">NEWS</a></li>
+                <li><a href="MyProfile.php">PROFILE</a></li>
+                <li><a href="Aboutus.php">ABOUT</a></li>
                 <div class="dropdown-menu">
 <button class="menu-btn">GAMES <i class="fas fa-angle-down"></i></button>
 <div class="menu-content">
-<a class="links-hidden" href="GamePage2.php">ATLAS</a>
+<a class="links-hidden" href="GamePage2.php" >ATLAS</a>
 <a class="links-hidden" href="GamePage.php">ARK:Survival Evolved</a>
 
 </div>
 </div>
                 
-                <li><a href="#">JOBS</a></li>
+                <li><a href="Jobs.php">JOBS</a></li>
                 <li><a href="Contact Us.php">CONTACT US</a></li>
             </ul>
         </nav>
          <input type="submit" class="button"onclick="document.location='signin.php'" value="Log in" name="submit">
-         <input type="submit" class="button1" onclick="document.location='regesteration.php'" value="Regstier" name="submit">
+         <input type="submit" class="Register-button" onclick="document.location='regesteration.php'" value="Regstier" name="submit">
     </header>
     </html>
